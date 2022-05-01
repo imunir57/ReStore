f06f7c8d758c544a803f1e16b4cf095e30836ccd
Munirul Islam <imunir57@gmail.com>
2022-05-02 00:32:39 +0600

    5th chapter

---
+++ ReStore/API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+                return NotFound();
+            return product;
+        }
+    }
+}
---
+++ ReStore/client/src/app/layout/App.tsx
@@ -0,0 +1,47 @@
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType == 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar/>
+    <CssBaseline/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/catalog' element={<Catalog/>}/>
+          <Route path='/catalog/:id' element={<ProductDetails/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/contact' element={<ContactPage/>}/>
+        </Routes>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
